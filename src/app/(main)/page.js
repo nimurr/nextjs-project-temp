0825914a5f6388f748c13a51836586b03f7ccdda
nimurr <nimurnerob404@gmail.com>
2025-11-26 16:24:53 +0600

    `Updated page.js: changed text size, added class to paragraph, and increased margin`

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -9,13 +9,13 @@ const Page = () => {
 
     return (
         <div className='container mx-auto'>
-            <h2 className='text-2xl font-semibold text-center my-5'>Demo Api Data Get For Check Rediux </h2>
+            <h2 className='text-5xl font-semibold text-center my-10'>Demo Api Data Get For Check Rediux </h2>
             <div className='grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>
                 {
                     data?.map((item) => (
                         <div className='bg-purple-500 hover:bg-purple-600 rounded-lg p-5 text-white' key={item.id}>
                             <h1 className='font-semibold mb-2 capitalize'>{item.title}</h1>
-                            <p>{item.body}</p>
+                            <p className='text-gray-300 capitalize'>{item.body}</p>
                         </div>
                     ))
                 }
