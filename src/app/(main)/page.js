6bc3633d03e75a0e08b225a47e54945e5418e6c6
nimurr <nimurnerob404@gmail.com>
2025-11-26 16:22:51 +0600

    Demo Api Data Get For Check Rediux

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -8,15 +8,18 @@ const Page = () => {
     console.log(data)
 
     return (
-        <div className='grid xl:grid-cols-6 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>
-            {
-                data?.map((item) => (
-                    <div className='bg-purple-500 hover:bg-purple-600 rounded-lg p-5 text-white' key={item.id}>
-                        <h1 className='font-semibold mb-2 capitalize'>{item.title}</h1>
-                        <p>{item.body}</p>
-                    </div>
-                ))
-            }
+        <div>
+            <h2 className='text-xl font-semibold text-center my-5'>Demo Api Data Get For Check Rediux </h2>
+            <div className='grid xl:grid-cols-6 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>
+                {
+                    data?.map((item) => (
+                        <div className='bg-purple-500 hover:bg-purple-600 rounded-lg p-5 text-white' key={item.id}>
+                            <h1 className='font-semibold mb-2 capitalize'>{item.title}</h1>
+                            <p>{item.body}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 }
