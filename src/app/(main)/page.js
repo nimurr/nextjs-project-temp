cfe328ab0340da1db730fce299c00d1948ac58e9
nimurr <nimurnerob404@gmail.com>
2025-12-01 09:10:47 +0600

    `Added loading state indicator to Page component`

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Page = () => {
 
-    const { data } = useGetDemoDataQuery();
+    const { data, isLoading } = useGetDemoDataQuery();
     console.log(data)
 
     return (
@@ -16,6 +16,30 @@ const Page = () => {
                         <div className='bg-purple-500 hover:bg-purple-600 rounded-lg p-5 text-white' key={item.id}>
                             <h1 className='font-semibold mb-2 capitalize'>{item.title}</h1>
                             <p className='text-gray-300 capitalize'>{item.body}</p>
+                        </div>
+                    ))
+                }
+                {
+                    isLoading &&
+                    [...Array(12)].map((item) => (
+                        <div class="mx-auto w-full max-w-sm rounded-md border border-purple-500 p-4">
+                            <div class="flex animate-pulse space-x-4">
+                                <div class="size-10 rounded-full bg-gray-200"></div>
+                                <div class="flex-1 space-y-6 py-1">
+                                    <div class="h-2 rounded bg-gray-200"></div>
+                                    <div class="h-2 rounded bg-gray-200"></div>
+                                    <div class="h-2 rounded bg-gray-200"></div>
+                                    <div class="space-y-3">
+                                        <div class="grid grid-cols-3 gap-4">
+                                            <div class="col-span-2 h-2 rounded bg-gray-200"></div>
+                                            <div class="col-span-1 h-2 rounded bg-gray-200"></div>
+                                        </div>
+                                        <div class="h-2 rounded bg-gray-200"></div>
+                                        <div class="h-2 rounded bg-gray-200"></div>
+                                        <div class="h-2 rounded bg-gray-200"></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     ))
                 }
